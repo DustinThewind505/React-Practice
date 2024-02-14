77eece6caca1023385445cb8edd6ae3ad21750d4
unknown <Dustin.Guillen14@yahoo.com>
2024-02-14 11:19:26 -0700

    [ticket-number] added data, polished functionality of single accordian selection for page5.js

--- react-practice/src/components/PAGE5/page5.js
+++ react-practice/src/components/PAGE5/page5.js
@@ -1,22 +1,37 @@
 import React, { useState } from 'react';
+import avengersData from './data.js'
 
+import './page5.css'
 
 function Page5() {
     // ===== STATE =====
-    const [firstAccordian, setFirstAccordian] = useState(false)
+    const [singleAccordianSelection, setSingleAccordianSelection] = useState(false);
 
     // ===== FUNCTIONS =====
+    const handleSingleSelection = (id) => {
+        setSingleAccordianSelection(id === singleAccordianSelection ? null : id)
+    }
 
 
     // ===== COMPONENT =====
     return(
         <div>
             <h1>Page 5</h1>
-            <div>
-                <p>First Accordian</p>
-                <span onClick={() => setFirstAccordian(!firstAccordian)}>+</span>
-                {firstAccordian ? <p>Test text</p> : null}
-            </div>
+            {
+                avengersData && avengersData.length > 0 ?
+                avengersData.map(avenger => <div>
+                    <p>{avenger.nickname}</p>
+                    <p onClick={() => handleSingleSelection(avenger.id)}>+</p>
+                    {
+                        singleAccordianSelection === avenger.id ? <div>
+                            <img src={avenger.img} alt={`Headshot of ${avenger.name}`} />
+                            <p>{avenger.description}</p>
+                        </div> : null
+                    }
+                </div>)
+                :
+                <p>No Avengers data found</p>
+            }
         </div>
     )
 
