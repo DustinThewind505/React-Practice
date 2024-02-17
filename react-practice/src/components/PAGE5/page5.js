58438776464ca8282d2ea0a02c42fa3c0290c3cb
unknown <Dustin.Guillen14@yahoo.com>
2024-02-16 19:18:00 -0700

    [ticket-number] added multiple selections functionality to accordian

--- react-practice/src/components/PAGE5/page5.js
+++ react-practice/src/components/PAGE5/page5.js
@@ -8,10 +8,31 @@ function Page5() {
     const [singleAccordianSelection, setSingleAccordianSelection] = useState(false);
     const [accordianIcon, setAccordianIcon] = useState(true);
 
+    const [enableMulitpleselection, setEnableMulitpleselection] = useState(false);
+    const [multiple, setMultiple] = useState([]);
+
     // ===== FUNCTIONS =====
     const handleSingleSelection = (id) => {
         setSingleAccordianSelection(id === singleAccordianSelection ? null : id)
         setAccordianIcon(id === singleAccordianSelection ? setAccordianIcon(!accordianIcon) : setAccordianIcon(true))
+    }
+
+    const handleMultiSelection = (id) => {
+        const newMultiple = [...multiple];
+        const findIndexOfId = newMultiple.indexOf(id);
+
+        if(findIndexOfId === -1) {
+            newMultiple.push(id);
+        } else {
+            newMultiple.splice(findIndexOfId, 1)
+        }
+        setMultiple(newMultiple);
+    }
+
+    const handleMultipleSelectionButton = () => {
+        setEnableMulitpleselection(!enableMulitpleselection);
+        setSingleAccordianSelection(false);
+        setMultiple([])
     }
 
 
@@ -19,13 +40,14 @@ function Page5() {
     return(
         <div>
             <h1>Page 5</h1>
+            <button onClick={() => handleMultipleSelectionButton}>{!enableMulitpleselection ? "Enable Multiple Selections" : "Disable Multiple Selections"}</button>
             {
                 avengersData && avengersData.length > 0 ?
                 avengersData.map(avenger => <div className="accordian-item">
                     <p>{avenger.nickname}</p>
-                    {singleAccordianSelection === avenger.id ? <p onClick={() => handleSingleSelection(avenger.id)}>-</p> : <p onClick={() => handleSingleSelection(avenger.id)}>+</p>}
+                    <p onClick={enableMulitpleselection ? () => handleMultiSelection(avenger.id) : () => handleSingleSelection(avenger.id)}>+</p>
                     {
-                        singleAccordianSelection === avenger.id ? <div>
+                        singleAccordianSelection === avenger.id  || multiple.indexOf(avenger.id) !== -1 ? <div>
                             <img src={avenger.img} alt={`Headshot of ${avenger.name}`} />
                             <p className="accordian-description">{avenger.description}</p>
                         </div> : null
