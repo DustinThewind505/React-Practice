2d4bc5dbb11923f8badc094beced475ee02e0409
unknown <Dustin.Guillen14@yahoo.com>
2024-02-18 07:03:59 -0700

    [ticket-number] added functionality to Random Color Generator page

--- react-practice/src/components/PAGE6/page6.js
+++ react-practice/src/components/PAGE6/page6.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 
 function Page6() {
 // ===== STATE =====
+const [backgroundColor, setBackgroundcolor] = useState("green");
 
 
 // ===== FUNCTIONS =====
+const randomColor = () => {
+    let hex = "#"
+    const hexArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F",]
+
+    for(let i = 0; i < 6; i++) {
+        hex += hexArray[Math.floor(Math.random() * hexArray.length)];
+    }
+    setBackgroundcolor(hex)
+}
 
 
 // ===== COMPONENT =====
@@ -15,6 +25,10 @@ function Page6() {
         <div>
             <h1>Page 6</h1>
             <p>Random Color Generator</p>
+            <div style={{backgroundColor: backgroundColor}}>
+                <p>Changing background</p>
+                <button onClick={randomColor}>Random Color</button>
+            </div>
         </div>
     )
 };
