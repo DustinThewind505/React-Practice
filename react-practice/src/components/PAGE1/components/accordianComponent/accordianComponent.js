ee62b1d3bd14aebfd54af7d4206709863d806ed5
Dustin G <Dustin.Guillen14@yahoo.com>
2024-03-28 18:45:39 -0600

    [ticket-numer] enabled multiple selection functionality on page 1 accordian componenet

--- react-practice/src/components/PAGE1/components/accordianComponent/accordianComponent.js
+++ react-practice/src/components/PAGE1/components/accordianComponent/accordianComponent.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
-import { CardHeader, CardBody, CardText } from "reactstrap";
+import { CardHeader, CardBody, CardText, Button } from "reactstrap";
 import mockData from "./data";
 
 
 function AccordianComponent() {
     const [accordianSingleSelection, setAccordianSingleSelection] = useState(0);
+    const [accordianMultipleSelection, setAccordianMultipleSelection] = useState([]);
+
+    const [enableMultipleSelection, setEnableMultipleSelection] = useState(false);
+
 
 
     const handleSingleSelection = (id) => {
@@ -16,19 +20,45 @@ function AccordianComponent() {
         }
         
     }
+    
+    const handleMultipleSelection = (id) => {
+        let newArray = [...accordianMultipleSelection];
+        const selectionIndex = newArray.indexOf(id);
+
+        if(selectionIndex === -1) {
+            newArray.push(id)
+        } else {
+            newArray.splice(selectionIndex, 1);
+        }
+
+        setAccordianMultipleSelection(newArray);
+
+        console.log(newArray)
+    }
+
+    const handleEnableButton = () => {
+        setAccordianSingleSelection(0);
+        setAccordianMultipleSelection([]);
+
+        setEnableMultipleSelection(!enableMultipleSelection);
+    }
+    
 
     return(
         <>
             <CardHeader>Accordian component</CardHeader>
+            <Button onClick={() => handleEnableButton()}>{!enableMultipleSelection ? "Enable Multiple Seletion" : "Disable Multiple Selection"}</Button>
+            <p>{enableMultipleSelection ? "Multiple selection enabled ✅" : null}</p>
             {
-                mockData || mockData.length !== 0 ?
+                mockData || mockData.length > 0 ?
+
                     mockData.map(data => {
                         
                         return(
-                            <CardBody key={data.id}>
-                                <CardText onClick={() => handleSingleSelection(data.id)}>{data.title} +</CardText>
+                            <CardBody key={data.id} onClick={enableMultipleSelection ? () => handleMultipleSelection(data.id) : () => handleSingleSelection(data.id)}>
+                                <CardText>{data.title} +</CardText>
                                 {
-                                    accordianSingleSelection === data.id ?
+                                    accordianSingleSelection === data.id || accordianMultipleSelection.indexOf(data.id) !== -1 ?
                                         <CardText>{data.body}</CardText>
                                     :
                                     null
