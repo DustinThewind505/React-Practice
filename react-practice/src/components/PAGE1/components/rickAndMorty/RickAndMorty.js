a3ff8dca90ef1a7e1d0da2b1222f08c1ff5cd163
Dustin G <Dustin.Guillen14@yahoo.com>
2024-04-04 17:43:38 -0600

    [ticket-number] added css and handler funtions to page 1 rick and morty api card

--- react-practice/src/components/PAGE1/components/rickAndMorty/RickAndMorty.js
+++ react-practice/src/components/PAGE1/components/rickAndMorty/RickAndMorty.js
@@ -7,8 +7,9 @@ import axios from 'axios';
 function RickAndMortyApi({ fontStyles, logo }) {
     const [data, setData] = useState([]);
     const [characterNumber, setCharacterNumber] = useState(826);
+    const [previousCharacterNumber, setPreviousCharacterNumber] = useState([]);
     const [pageNumber, setPageNumber] = useState(1);
-    const [pageData, setPageData] = useState([])
+    const [pageData, setPageData] = useState([]);
 
 
 
@@ -25,7 +26,14 @@ function RickAndMortyApi({ fontStyles, logo }) {
     }
 
     const handleRandom = () => {
+        const newArray = [...previousCharacterNumber]
+        newArray.push(characterNumber)
+        setPreviousCharacterNumber(newArray)
         setCharacterNumber(Math.floor(Math.random() * 826))
+    }
+
+    const handlePreviousRandom = () => {
+        setCharacterNumber(previousCharacterNumber.pop())
     }
 
     useEffect(() => {
@@ -55,10 +63,15 @@ function RickAndMortyApi({ fontStyles, logo }) {
         <>
             <CardHeader><img src={logo} className="App-logo App-logo-page-1" alt="logo" />Rick and Morty <span style={fontStyles}>API</span><img src={logo} className="App-logo App-logo-page-1" alt="logo" /></CardHeader>
             <CardBody>
-                <CardText>{data.name}</CardText>
-                <img src={data.image} alt="morty smith" />
-                <div className="rick-and-morty-buttons">
-                    <Button onClick={handleRandom}>Random</Button>
+                <div className="random-rick-and-morty-card">
+                    <CardText>{data.name}</CardText>
+                    <div className="random-rick-and-morty-card-bottom">
+                        <img src={data.image} alt="morty smith" />
+                        <div className="random-rick-and-morty-card-bottom-buttons">
+                            <Button onClick={handleRandom}>Random</Button>
+                            <Button onClick={handlePreviousRandom} disabled={previousCharacterNumber.length > 0 ? false : true}>Previous</Button>
+                        </div>
+                    </div>
                 </div>
                 <CardText>Page {pageNumber}</CardText>
                 <Button onClick={handlePrevious}>Previous Page</Button>
