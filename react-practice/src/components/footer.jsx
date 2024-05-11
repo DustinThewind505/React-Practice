d24a05cd684ae937abb85c0883aa852e9534ee1c
Dustin G <Dustin.Guillen14@yahoo.com>
2024-05-10 21:29:25 -0600

    [ticket-number] completed footer component, css fixes for app background

--- react-practice/src/components/footer.jsx
+++ react-practice/src/components/footer.jsx
@@ -9,7 +9,18 @@ function Footer() {
 
     return(
         <footer>
-            <h1>Footer</h1>
+            <div className="left-section">
+                <img className="github-logo" src="/images/reactLogo.png" alt="github logo" />
+            </div>
+            <div className="middle-section">
+                <p>Designed by Dustin Guillen</p>
+            </div>
+            <div className="right-section">
+                <a href="https://github.com/DustinThewind505/react-practice">
+                    <img className="github-logo" src="https://cdn3.iconfinder.com/data/icons/ultimate-social/150/48_github-512.png" alt="github logo" />
+                    <p>View Code</p>
+                </a>
+            </div>
         </footer>
     )
 }
